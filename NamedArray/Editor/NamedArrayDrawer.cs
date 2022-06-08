131599df37a25d1954295635f982cf68acf9f375
Curt Liu <System3748@outlook.com>
2022-06-08 20:22:47 +0800

    Update NamedArrayDrawer.cs
    Add New Display Mode - Customized format display

--- NamedArray/Editor/NamedArrayDrawer.cs
+++ NamedArray/Editor/NamedArrayDrawer.cs
@@ -1,46 +1,60 @@
-﻿using System;
+using System;
 using UnityEditor;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
 public class NamedArrayDrawer : PropertyDrawer
 {
-	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-	{
-		var height = EditorGUI.GetPropertyHeight(property, label, true);
-		if (property.propertyType == SerializedPropertyType.Enum)
-			height *= 2;
-		return height;
-	}
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        var height = EditorGUI.GetPropertyHeight(property, label, true);
+        if (property.propertyType == SerializedPropertyType.Enum)
+            height *= 2;
+        return height;
+    }
 
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-	{
-		NamedArrayAttribute namedInfo = attribute as NamedArrayAttribute;
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        NamedArrayAttribute namedInfo = attribute as NamedArrayAttribute;
 
-		int index = Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", ""));
-		//change the label
+        int index = Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", ""));
+        //change the label
 
-		if (namedInfo.customName != null)
-		{
-			label.text = namedInfo.customName + "-" + index;
-		}
-		else if (index + namedInfo.enumOffest >= namedInfo.enumNames.Length)
-		{
-			label.text = "No EnumIndex:" + index;
-		}
-		else
-		{
-			if (namedInfo.prefix != null)
-			{
-				label.text = namedInfo.prefix + "::" + namedInfo.enumNames[index + namedInfo.enumOffest];
-			}
-			else
-			{
-				label.text = namedInfo.enumNames[index + namedInfo.enumOffest];
-			}
-		}
+        if (namedInfo.customName != null)
+        {
+            if (namedInfo.fieldName != null)
+            {
+                label.text = $"{namedInfo.customName} {index} [{property.FindPropertyRelative(namedInfo.fieldName)?.stringValue}]";
+            }
+            else
+            {
+                if (namedInfo.isFormat)
+                    label.text = string.Format(namedInfo.customName, index + namedInfo.enumOffest);
+                else
+                    label.text = $"{namedInfo.customName}-{index + namedInfo.enumOffest}";
+            }
+        }
+        else if (namedInfo.fieldName != null)
+        {
+            label.text = $"Element {index} [{property.FindPropertyRelative(namedInfo.fieldName)?.stringValue}]";
+        }
+        else if (index + namedInfo.enumOffest >= namedInfo.enumNames.Length)
+        {
+            label.text = "No EnumIndex:" + index;
+        }
+        else
+        {
+            if (namedInfo.prefix != null)
+            {
+                label.text = namedInfo.prefix + "::" + namedInfo.enumNames[index + namedInfo.enumOffest];
+            }
+            else
+            {
+                label.text = namedInfo.enumNames[index + namedInfo.enumOffest];
+            }
+        }
 
-		//draw field
-		EditorGUI.PropertyField(position, property, label, true);
-	}
+        //draw field
+        EditorGUI.PropertyField(position, property, label, true);
+    }
 }
